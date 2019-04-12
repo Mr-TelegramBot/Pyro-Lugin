c183bd9a189bc055fe5841c3eddca49a33b1e592
MrMahdi313 <m.m.z.m12363@gmail.com>
2019-04-12 14:54:37 +0430

    Config.py Was Modified

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
-token = '415948872:AAGOb2f5TiGu_r1OPCcXpv0APiN5A_B2z0E'
-api_id = 122349
-api_hash = 'd0eae310e304a5f4fb97b9cf4849175d'
+token = ''  # Should Be String
+api_id =    # Should Be Integer
+api_hash = ''  # Should Be String
